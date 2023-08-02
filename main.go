a474aadbc9e742e1376551a20043107b9d5010b7
rosercode <rosercode@gmail.com>
2023-08-02 12:44:17 +0800

    feat Read Configuration From Ini File

--- main.go
+++ main.go
@@ -6,21 +6,20 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"strings"
+
+	"github.com/go-ini/ini"
 )
 
 // Define reverse proxy mapping table
-var proxyMap = map[string]*url.URL{
-	"test1.example.com": parseURL("http://localhost:8000"), // Forward test1.example.com to local HTTP server (port 8000)
-	"test2.example.com": parseURL("http://localhost:9000"), // Forward test2.example.com to local HTTP server (port 9000)
-}
+var proxyMap = make(map[string]*url.URL)
 
 // Parse URL
-func parseURL(rawurl string) *url.URL {
+func parseURL(rawurl string) (*url.URL, error) {
 	parsedURL, err := url.Parse(rawurl)
 	if err != nil {
-		log.Fatalf("Error parsing URL: %s", err)
+		return nil, err
 	}
-	return parsedURL
+	return parsedURL, nil
 }
 
 // Custom reverse proxy handler
@@ -30,6 +29,11 @@ func (rph *reverseProxyHandler) ServeHTTP(w http.ResponseWriter, req *http.Reque
 	// Parse the requested domain
 	hostParts := strings.Split(req.Host, ":")
 	domain := hostParts[0]
+
+	// Print request log
+	log.Println("Received request for domain:", domain)
+	log.Println("Request URL:", req.URL.String())
+	log.Println("Request Method:", req.Method)
 
 	// Look up the corresponding target URL in the proxy mapping table
 	targetURL, ok := proxyMap[domain]
@@ -56,15 +60,41 @@ func (rph *reverseProxyHandler) ServeHTTP(w http.ResponseWriter, req *http.Reque
 }
 
 func main() {
-	// Print the address and port that the server is listening on
-	addr := ":18080"
-	log.Printf("Reverse proxy server listening on %s...", addr)
-
-	// Print the reverse proxy mapping table
-	log.Println("Reverse proxy mapping table:")
-	for domain, targetURL := range proxyMap {
-		log.Printf("%s => %s", domain, targetURL.String())
+	// Load Configuration File
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Fatalf("Failed to load configuration file: %v", err)
+		return
 	}
+
+	// Parse Reverse Proxy Mapping Table from Configuration File
+	section := cfg.Section("proxy")
+	if section == nil {
+		log.Fatal("Proxy section not found in configuration file")
+		return
+	}
+
+	// Iterate through Configuration Entries and Parse URLs
+	log.Println("Reverse proxy mapping table:")
+	for _, key := range section.Keys() {
+		targetURL, err := parseURL(key.String())
+		// Print the reverse proxy mapping table
+		log.Printf("%s => %s", key.Name(), targetURL.String())
+		if err != nil {
+			log.Fatalf("Failed to parse URL for key %s: %v", key, err)
+		}
+		proxyMap[key.Name()] = targetURL
+	}
+
+	// Read Server Configuration
+	serverSection := cfg.Section("server")
+	if serverSection == nil {
+		log.Fatal("Server section not found in configuration file")
+		return
+	}
+
+	address := serverSection.Key("address").String()
+	port := serverSection.Key("port").String()
 
 	// Create an instance of the custom reverse proxy handler
 	handler := &reverseProxyHandler{}
@@ -73,7 +103,9 @@ func main() {
 	http.Handle("/", handler)
 
 	// Start the server
-	err := http.ListenAndServe(addr, nil)
+	addr := address + ":" + port
+	log.Printf("Reverse proxy server listening on %s...", addr)
+	err = http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
